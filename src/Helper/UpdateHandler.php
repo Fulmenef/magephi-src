7da8a32fa7ac363b33d8eaf38f5964c195b2101b
Axel Perroud <ape@emakina.fr>
2021-10-24 01:15:14 +0200

    Push new phar to repository

--- src/Helper/UpdateHandler.php
+++ src/Helper/UpdateHandler.php
@@ -21,19 +21,20 @@ class UpdateHandler
      */
     public function handle(): ?string
     {
-        $updater = new Updater(null, false);
-        $strategy = new GithubStrategy();
-        $strategy->setPackageName(self::PACKAGE_NAME);
-        $strategy->setPharName(self::FILE_NAME);
-        $strategy->setCurrentLocalVersion(Kernel::getVersion());
-        $updater->setStrategyObject($strategy);
-
-        $result = $updater->update();
-
-        if (!$result) {
-            $updater->rollback();
-        }
-
-        return $result ? $updater->getNewVersion() : null;
+        dump('TODO Remove and replace by check ?');
+//        $updater = new Updater(null, false);
+//        $strategy = new GithubStrategy();
+//        $strategy->setPackageName(self::PACKAGE_NAME);
+//        $strategy->setPharName(self::FILE_NAME);
+//        $strategy->setCurrentLocalVersion(Kernel::getVersion());
+//        $updater->setStrategyObject($strategy);
+//
+//        $result = $updater->update();
+//
+//        if (!$result) {
+//            $updater->rollback();
+//        }
+//
+        return null;
     }
 }
