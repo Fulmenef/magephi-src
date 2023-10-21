e02125db6311530d0bdda446499b41ab4d9aa482
Axel Perroud <ape@emakina.fr>
2023-10-21 16:42:12 +0200

    Add restart command and option for build command

--- src/Command/Environment/BuildCommand.php
+++ src/Command/Environment/BuildCommand.php
@@ -7,6 +7,7 @@ namespace Magephi\Command\Environment;
 use Magephi\Exception\EnvironmentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -14,6 +15,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class BuildCommand extends AbstractEnvironmentCommand
 {
+    private const OPTION_RESTART = 'restart';
+
     protected string $command = 'build';
 
     protected function configure(): void
@@ -23,7 +26,8 @@ class BuildCommand extends AbstractEnvironmentCommand
             ->setDescription('Build docker containers, equivalent to <fg=yellow>make build</>')
             ->setHelp(
                 'This command allows you to build container for your Magento 2 environment.'
-            );
+            )
+            ->addOption(self::OPTION_RESTART, null, InputOption::VALUE_NONE, 'Use this option to restart the environment after the build');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -41,6 +45,44 @@ class BuildCommand extends AbstractEnvironmentCommand
 
         $this->interactive->newLine(2);
         $this->interactive->success('Containers have been built.');
+
+        if ($input->getOption(self::OPTION_RESTART)) {
+            $this->interactive->section('Stopping environment');
+
+            try {
+                $this->manager->stop();
+            } catch (\Exception $e) {
+                $this->interactive->newLine(2);
+                $this->interactive->error(
+                    [
+                        "Environment couldn't be stopped:",
+                        $e->getMessage(),
+                    ]
+                );
+
+                return self::FAILURE;
+            }
+
+            $this->interactive->newLine(2);
+            $this->interactive->section('Starting environment');
+
+            try {
+                $this->manager->start();
+            } catch (\Exception $e) {
+                $this->interactive->newLine(2);
+                $this->interactive->error(
+                    [
+                        "Environment couldn't be started:",
+                        $e->getMessage(),
+                    ]
+                );
+
+                return self::FAILURE;
+            }
+
+            $this->interactive->newLine(2);
+            $this->interactive->success('Environment started.');
+        }
 
         return self::SUCCESS;
     }
